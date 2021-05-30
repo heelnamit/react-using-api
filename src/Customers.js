79323a8c8dd45f049e569bf9679b7b7afe832e7b
heelnamit <63186182+heelnamit@users.noreply.github.com>
2021-05-30 12:28:33 +0530

    Add files via upload

---
+++ src/Customers.js
@@ -0,0 +1,62 @@
+import React,{useState} from 'react';
+import axios from 'axios';
+import classes from './Card.module.css';
+import { Pagination } from './Pagination';
+import { CustomerCard } from './CustomerCard';
+import Button from '@material-ui/core/Button';
+
+
+const Card = () =>{
+    const [loading, setLoading] = useState(false);
+    const [customers, setCustomers] =useState([]);
+    const [currentPage, setCurrentPage] =useState(1);
+    const [customerPerPage, setCustomerPerPage] =useState(5);
+
+    const apiURL="https://intense-tor-76305.herokuapp.com/merchants";
+    
+    const fetchData = async () => {
+        const response = await axios.get(apiURL)
+        setLoading(true)
+        setCustomers(response.data)
+    }
+
+    let indexOfLastPost = currentPage * customerPerPage;
+    let indexOfFirstPost = indexOfLastPost - customerPerPage;
+    let currentCustomers = customers.slice(indexOfFirstPost, indexOfLastPost);
+
+    return ( 
+        
+    <div className={classes.outer}>
+      <h1>Customer Information</h1>
+      <h2>Fetch a list from an API and display it</h2>
+
+      <div className={classes.button}>
+        <Button variant="contained" color="primary" onClick={fetchData}>
+          Fetch Data
+        </Button>
+      </div>
+      { customers.length > 0 &&
+        <Pagination 
+            totalCustomers={customers.length}
+            customerPerPage={customerPerPage}
+            onPageClick={setCurrentPage}
+            currentPage={currentPage}
+        />
+        }
+      <div className={classes.inner}>
+          {
+             currentCustomers && currentCustomers.map((customer,index)=>{
+                 return(
+                     <CustomerCard 
+                        customer={customer}
+                        index={index}
+                     />
+                 )
+             })
+          }
+      </div>
+       
+    </div> );
+}
+
+export default Card;
